a2ab13da40a65af10006791e70ab279ef9122862
martin <martin@pythonic.eu>
2010-10-03 20:29:32 +0200

    Removed ignore from symlink script

--- symlinks.sh
+++ symlinks.sh
@@ -2,7 +2,7 @@
 
 # Create symlinks for all dotfiles
 
-DOTFILES=$(ls --almost-all -1 --ignore='.git' --ignore='symlinks.sh' --ignore='laptop' $PWD)
+DOTFILES=$(ls --almost-all -1 --ignore='.git' --ignore='symlinks.sh' $PWD)
 
 for DOTFILE in $DOTFILES; do
     ln -s -f $PWD/$DOTFILE $HOME/$DOTFILE
