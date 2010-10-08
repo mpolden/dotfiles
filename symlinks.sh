95e44d00f39692488cdf86f0478bddf8229df160
martin <martin@pythonic.eu>
2010-10-08 12:49:24 +0200

    Added --force flag to force overwrite of existing dotfiles in ~

--- symlinks.sh
+++ symlinks.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 # Create symlinks for all dotfiles
+# Use --force to overwrite existing dotfiles
 
 DOTFILES=$(ls --almost-all -1 --ignore='.git' --ignore='symlinks.sh' $PWD)
 
+LN_OPTS="-s"
+if [[ "$1" == "--force" ]]; then
+	LN_OPTS="$LN_OPTS -f"
+fi
+
 for DOTFILE in $DOTFILES; do
-    ln -s -f $PWD/$DOTFILE $HOME/$DOTFILE
+    ln $LN_OPTS $PWD/$DOTFILE $HOME/$DOTFILE
 done
 
 exit $?
