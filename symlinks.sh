a16a59c3334351ad8edaed15478568296fba49bb
Martin Polden <martin.polden@gmail.com>
2011-06-27 19:03:38 +0200

    Remove dot.ssh handling from symlinks script

--- symlinks.sh
+++ symlinks.sh
@@ -1,23 +1,19 @@
 #!/usr/bin/env bash
 
 # Create symlinks for all dotfiles
-# Use --force to overwrite existing dotfiles
+# Use -f to overwrite existing dotfiles
 
 DOTFILES=$(ls -1 -d $PWD/dot.*)
 
 LN_OPTS="-s"
-if [[ "$1" == "--force" ]]; then
+if [[ "$1" == "-f" ]]; then
 	LN_OPTS="$LN_OPTS -f"
 fi
 
 for DOTFILE in $DOTFILES; do
     BASENAME=$(basename $DOTFILE)
-    if [[ "$BASENAME" == "dot.ssh" ]]; then
-        ln $LN_OPTS $DOTFILE/config $HOME/.ssh/config
-    else
-        TARGET="$HOME/${BASENAME/dot/}"
-        ln $LN_OPTS $DOTFILE $TARGET
-    fi
+    TARGET="$HOME/${BASENAME/dot/}"
+    ln $LN_OPTS $DOTFILE $TARGET
 done
 
 exit $?
