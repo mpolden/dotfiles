7f6be695266f7a3bb8f2895ca893ba4a40c2c0e0
Martin Polden <martin.polden@gmail.com>
2013-08-29 09:08:08 +0200

    Check if pane_pid has children before sourcing zshrc

--- tmux-rehash-panes.sh
+++ tmux-rehash-panes.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-IFS=$'\n'
-for LINE in $(tmux list-windows | awk '{print $1,$2}' | tr -d ':'); do
-    WIN=$(echo "$LINE" | cut -d ' ' -f 1) 
-    CMD=$(echo "$LINE" | cut -d ' ' -f 2) 
-    if [[ "$CMD" == zsh* ]]; then
-        tmux send -t 0:${WIN}.0 'source ~/.zshrc' ENTER
+for LINE in $(tmux list-panes -aF '#{session_name}:#{window_index}.#{pane_index},#{pane_pid}'); do
+    PANE=$(echo "$LINE" | cut -d ',' -f 1)
+    PANE_PID=$(echo "$LINE" | cut -d ',' -f 2)
+    ps --no-headers -o pid --ppid=$PANE_PID > /dev/null
+    if [[ $? -ne 0 ]]; then
+        tmux send-keys -t $PANE 'source ~/.zshrc' enter
     fi
 done
-unset IFS
 
 exit $?
