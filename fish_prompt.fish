8cf4c07b6ef56cf913e454de2bfa9686520e29dd
Martin Polden <mpolden@mpolden.no>
2024-01-04 13:14:24 +0100

    fish: port current zsh config

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,35 +1,32 @@
-function fish_prompt --description 'Write out the prompt'
+function fish_prompt --description "Write out the prompt"
+        set last_status $status
         if not set -q __fish_prompt_hostname
                 set -g __fish_prompt_hostname (hostname -s)
         end
 
+        set normal (set_color normal)
         set -q fish_color_at
         or set -U fish_color_at $normal
 
-        set -l normal (set_color normal)
-        set -l ssh_prefix
-        if set -q SSH_TTY
+        # Add user@host when connected through SSH or using toolbox
+        set ssh_prefix
+        if set -q SSH_CLIENT
+            or set -q SSH_TTY
+            or set -q SSH_CONNECTION
+            or set -q TOOLBOX_PATH
                 set ssh_prefix (set_color $fish_color_user) $USER \
                 (set_color $fish_color_at) @ (set_color $fish_color_host) \
-                $__fish_prompt_hostname $normal ':'
+                $__fish_prompt_hostname $normal ":"
         end
 
-        # Display fancy symbol on darwin
+        # Color prompt symbol based on exit status. Inspired by
+        # https://solovyov.net/blog/2020/useful-shell-prompt/
         set __prompt_symbol '$'
-        if [ -z "$EMACS" -a (uname) = 'Darwin' ]
-                set -l burger (echo -ne '\xF0\x9F\x8D\x94')
-                set -l coffee (echo -ne '\xE2\x98\x95')
-                set -l beer (echo -ne '\xF0\x9F\x8D\xBA')
-                set -l hour (date +%-k)
-                if [ $hour -ge 8 -a $hour -lt 16 ]
-                        set __prompt_symbol "$coffee "
-                else if [ $hour -ge 16 -a $hour -lt 19 ]
-                        set __prompt_symbol "$burger "
-                else
-                        set __prompt_symbol "$beer "
-                end
+        if [ $last_status -ne 0 ]
+            set __prompt_symbol "$(set_color yellow)\$$normal"
         end
 
+        # Print prompt
         echo -n -s $ssh_prefix (set_color $fish_color_cwd) (prompt_pwd) \
-        $normal (__fish_git_prompt ' %s') $normal $__prompt_symbol ' '
+            $normal (__fish_git_prompt " %s") $normal $__prompt_symbol " "
 end
