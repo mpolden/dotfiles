7a5f695ecc7141b95a57395bc20582988ea5cb23
Martin Polden <mpolden@mpolden.no>
2025-03-31 15:55:43 +0200

    fish: add idea command to PATH

--- config.fish
+++ config.fish
@@ -43,7 +43,10 @@ end
 fish_add_path --prepend --move /usr/local/bin
 # Pre-Sonoma macOS lacks trash command
 fish_add_path --prepend --move /usr/local/opt/trash/bin
+# Lazy way of getting mvn: Use the one bundled with IDEA
 fish_add_path --prepend --move "/Applications/IntelliJ IDEA CE.app/Contents/plugins/maven/lib/maven3/bin"
+# Contains 'idea' command, for opening files in IDEA from CLI
+fish_add_path --prepend --move "/Applications/IntelliJ IDEA CE.app/Contents/MacOS"
 fish_add_path --prepend --move "$HOME/.local/bin"
 fish_add_path --prepend --move "$HOME/.cargo/bin"
 fish_add_path --prepend --move "$HOME/go/bin"
